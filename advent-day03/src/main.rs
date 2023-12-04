d3a8a02b83f584cec1ca7c697b29955302408a86
JD Huntington <jd@itsthehuntingtons.com>
2023-12-04 11:53:39 -0600

    tests pass

--- advent-day03/src/main.rs
+++ advent-day03/src/main.rs
@@ -6,12 +6,14 @@ struct PartNumber {
     width: u32,
     y: u32,
     number: u32,
+    real_part: bool,
 }
 
 #[derive(Debug, PartialEq)]
 struct Part {
     x: u32,
     y: u32,
+    symbol: char,
 }
 
 fn main() {
@@ -20,17 +22,78 @@ fn main() {
 }
 
 fn process_input<R: Read>(reader: R) -> u32 {
+    let mut x_len = 0;
+    let mut y_len = 0;
+    let mut parts: Vec<Part> = Vec::new();
+    let mut part_numbers: Vec<PartNumber> = Vec::new();
     let buffered = io::BufReader::new(reader);
     for line_result in buffered.lines() {
         let line = line_result.unwrap();
         if line.trim().is_empty() {
             continue;
         }
+        process_line(&mut part_numbers, &mut parts, &line, y_len);
+        y_len += 1;
+        if x_len == 0 {
+            x_len = line.len() as u32;
+        }
     }
-    0
+
+    // Create a matrix of Option<index> where the index is the index of the part in the parts vector
+    let mut matrix: Vec<Vec<Option<u32>>> = Vec::new();
+    while matrix.len() <= y_len as usize {
+        let mut row = Vec::new();
+        while row.len() <= x_len as usize {
+            row.push(None);
+        }
+        matrix.push(row);
+    }
+    for (index, part_number) in part_numbers.iter().enumerate() {
+        for x in part_number.x..(part_number.x + part_number.width) {
+            matrix[part_number.y as usize][x as usize] = Some(index as u32);
+        }
+    }
+    for part in parts {
+        let x = part.x;
+        let y = part.y;
+        for x_offset in -1..=1 {
+            for y_offset in -1..=1 {
+                if x_offset == 0 && y_offset == 0 {
+                    continue;
+                }
+                let mut current_x = x as i32;
+                let mut current_y = y as i32;
+
+                current_x += x_offset;
+                current_y += y_offset;
+                if current_x < 0 || current_y < 0 {
+                    break;
+                }
+                let current_x = current_x as usize;
+                let current_y = current_y as usize;
+                if current_x >= matrix[0].len() || current_y >= matrix.len() {
+                    break;
+                }
+                if let Some(index) = matrix[current_y][current_x] {
+                    let part_number = &part_numbers[index as usize];
+                    if part_number.real_part {
+                        break;
+                    }
+                    part_numbers[index as usize].real_part = true;
+                }
+            }
+        }
+    }
+    let mut sum = 0;
+    for part_number in part_numbers {
+        if part_number.real_part {
+            sum += part_number.number;
+        }
+    }
+    sum
 }
 
-fn process_line(parts: &mut Vec<PartNumber>, line: &str, y_pos: u32) {
+fn process_line(part_numbers: &mut Vec<PartNumber>, parts: &mut Vec<Part>, line: &str, y_pos: u32) {
     let mut current_number = String::new();
     let mut current_x = 0;
     let mut current_width = 0;
@@ -39,28 +102,49 @@ fn process_line(parts: &mut Vec<PartNumber>, line: &str, y_pos: u32) {
             if !current_number.is_empty() {
                 let part = PartNumber {
                     x: current_x + 1 - current_width,
+                    real_part: false,
                     width: current_width,
                     y: y_pos,
                     number: current_number.parse::<u32>().unwrap(),
                 };
-                parts.push(part);
+                part_numbers.push(part);
                 current_number.clear();
                 current_width = 0;
             }
-        } else {
+        } else if c.is_digit(10) {
             current_number.push(c);
             current_width += 1;
             current_x = x_pos as u32;
+        } else {
+            if !current_number.is_empty() {
+                let part = PartNumber {
+                    x: current_x + 1 - current_width,
+                    real_part: false,
+                    width: current_width,
+                    y: y_pos,
+                    number: current_number.parse::<u32>().unwrap(),
+                };
+                part_numbers.push(part);
+                current_number.clear();
+                current_width = 0;
+            }
+            let part = Part {
+                x: x_pos as u32,
+                y: y_pos,
+                symbol: c,
+            };
+            parts.push(part);
         }
     }
     if !current_number.is_empty() {
         let part = PartNumber {
             x: current_x + 1 - current_width,
+            real_part: false,
             width: current_width,
             y: y_pos,
             number: current_number.parse::<u32>().unwrap(),
         };
-        parts.push(part);
+        part_numbers.push(part);
     }
 }
 
@@ -73,19 +157,21 @@ mod tests {
         let mut expected = Vec::new();
         expected.push(PartNumber {
             x: 0,
+            real_part: false,
             width: 3,
             y: 0,
             number: 467,
         });
         expected.push(PartNumber {
             x: 5,
+            real_part: false,
             width: 3,
             y: 0,
             number: 114,
         });
         let input = "467..114..";
         let mut parts: Vec<PartNumber> = Vec::new();
-        process_line(&mut parts, input, 0);
+        process_line(&mut parts, &mut Vec::new(), input, 0);
         assert_eq!(expected, parts);
     }
 
@@ -94,13 +180,14 @@ mod tests {
         let mut expected = Vec::new();
         expected.push(PartNumber {
             x: 0,
+            real_part: false,
             width: 1,
             y: 0,
             number: 1,
         });
         let input = "1";
         let mut parts: Vec<PartNumber> = Vec::new();
-        process_line(&mut parts, input, 0);
+        process_line(&mut parts, &mut Vec::new(), input, 0);
         assert_eq!(expected, parts);
     }
 
@@ -109,25 +196,28 @@ mod tests {
         let mut expected = Vec::new();
         expected.push(PartNumber {
             x: 0,
+            real_part: false,
             width: 1,
             y: 0,
             number: 1,
         });
         expected.push(PartNumber {
             x: 2,
+            real_part: false,
             width: 1,
             y: 0,
             number: 1,
         });
         expected.push(PartNumber {
             x: 4,
+            real_part: false,
             width: 1,
             y: 0,
             number: 1,
         });
         let input = "1.1.1";
         let mut parts: Vec<PartNumber> = Vec::new();
-        process_line(&mut parts, input, 0);
+        process_line(&mut parts, &mut Vec::new(), input, 0);
         assert_eq!(expected, parts);
     }
 
@@ -136,13 +226,14 @@ mod tests {
         let mut expected = Vec::new();
         expected.push(PartNumber {
             x: 0,
+            real_part: false,
             width: 5,
             y: 0,
             number: 12345,
         });
         let input = "12345";
         let mut parts: Vec<PartNumber> = Vec::new();
-        process_line(&mut parts, input, 0);
+        process_line(&mut parts, &mut Vec::new(), input, 0);
         assert_eq!(expected, parts);
     }
 
@@ -151,14 +242,97 @@ mod tests {
         let mut expected = Vec::new();
         expected.push(PartNumber {
             x: 0,
+            real_part: false,
             width: 5,
             y: 0,
             number: 12345,
         });
         let input = "12345.";
         let mut parts: Vec<PartNumber> = Vec::new();
-        process_line(&mut parts, input, 0);
+        process_line(&mut parts, &mut Vec::new(), input, 0);
         assert_eq!(expected, parts);
+    }
+
+    #[test]
+    fn test_parse_line6() {
+        let mut expected_parts: Vec<Part> = Vec::new();
+        let expected_part_numbers: Vec<PartNumber> = Vec::new();
+        expected_parts.push(Part {
+            x: 3,
+            y: 0,
+            symbol: '$',
+        });
+        expected_parts.push(Part {
+            x: 5,
+            y: 0,
+            symbol: '*',
+        });
+        let input = "...$.*....";
+        let mut part_numbers: Vec<PartNumber> = Vec::new();
+        let mut parts: Vec<Part> = Vec::new();
+        process_line(&mut part_numbers, &mut parts, input, 0);
+        assert_eq!(expected_parts, parts);
+        assert_eq!(expected_part_numbers, part_numbers);
+    }
+
+    #[test]
+    fn test_parse_line7() {
+        let mut expected_parts: Vec<Part> = Vec::new();
+        let mut expected_part_numbers: Vec<PartNumber> = Vec::new();
+        expected_parts.push(Part {
+            x: 3,
+            y: 4,
+            symbol: '*',
+        });
+        expected_part_numbers.push(PartNumber {
+            x: 0,
+            real_part: false,
+            width: 3,
+            y: 4,
+            number: 617,
+        });
+        let input = "617*......";
+        let mut part_numbers: Vec<PartNumber> = Vec::new();
+        let mut parts: Vec<Part> = Vec::new();
+        process_line(&mut part_numbers, &mut parts, input, 4);
+        assert_eq!(expected_parts, parts);
+        assert_eq!(expected_part_numbers, part_numbers);
+    }
+
+    #[test]
+    fn test_parse_line8() {
+        let mut expected_parts: Vec<Part> = Vec::new();
+        let mut expected_part_numbers: Vec<PartNumber> = Vec::new();
+        expected_parts.push(Part {
+            x: 3,
+            y: 4,
+            symbol: '*',
+        });
+        expected_parts.push(Part {
+            x: 7,
+            y: 4,
+            symbol: '*',
+        });
+        expected_part_numbers.push(PartNumber {
+            x: 0,
+            real_part: false,
+            width: 3,
+            y: 4,
+            number: 617,
+        });
+        expected_part_numbers.push(PartNumber {
+            x: 4,
+            real_part: false,
+            width: 3,
+            y: 4,
+            number: 617,
+        });
+        let input = "617*617*......";
+        let mut part_numbers: Vec<PartNumber> = Vec::new();
+        let mut parts: Vec<Part> = Vec::new();
+        process_line(&mut part_numbers, &mut parts, input, 4);
+        assert_eq!(expected_parts, parts);
+        assert_eq!(expected_part_numbers, part_numbers);
     }
 
     #[test]
