c522bb72528e5cc1c5bfe64dd00556bf4c0d7711
JD Huntington <jd@itsthehuntingtons.com>
2023-12-04 19:05:57 -0600

    Day 04

--- advent-day04/src/main.rs
+++ advent-day04/src/main.rs
@@ -1,22 +1,37 @@
 use std::io::{self, BufRead, Read};
 
 fn main() {
-    let (sum, unk) = process_input(io::stdin().lock());
-    println!("sum: {}, (?): {}", sum, unk);
+    let (sum, scratchcard_count) = process_input(io::stdin().lock());
+    println!("sum: {}, scratchcard_count: {}", sum, scratchcard_count);
 }
 
 fn process_input<R: Read>(reader: R) -> (u32, u32) {
     let buffered = io::BufReader::new(reader);
     let mut sum = 0;
+    let mut scratchcard_count = 0;
+    let mut multipliers: Vec<u32> = Vec::new();
     for line_result in buffered.lines() {
         let line = line_result.unwrap();
         if line.trim().is_empty() {
             continue;
         }
-        let (card_value, _unk) = process_line(&line);
+        let mut current_multiplier = 1;
+
+        if let Some(_first_element) = multipliers.get(0) {
+            let first_element = multipliers.remove(0);
+            current_multiplier += first_element;
+        }
+        scratchcard_count += current_multiplier;
+        let (card_value, counts) = process_line(&line);
+        for i in 1..=(counts as usize) {
+            if i > multipliers.len() {
+                multipliers.push(0);
+            }
+            multipliers[i - 1] += current_multiplier;
+        }
         sum += card_value;
     }
-    (sum, 0)
+    (sum, scratchcard_count)
 }
 
 fn process_line(line: &str) -> (u32, u32) {
@@ -46,7 +61,7 @@ fn process_line(line: &str) -> (u32, u32) {
     if winner_count == 0 {
         (0, 0)
     } else {
-        (1 << winner_count - 1, 0)
+        (1 << winner_count - 1, winner_count as u32)
     }
 }
 
@@ -58,28 +73,28 @@ mod tests {
     fn test_parse_line_1() {
         let line = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53";
         let score = process_line(line);
-        assert_eq!((8, 0), score);
+        assert_eq!((8, 4), score);
     }
 
     #[test]
     fn test_parse_line_2() {
         let line = "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19";
         let score = process_line(line);
-        assert_eq!((2, 0), score);
+        assert_eq!((2, 2), score);
     }
 
     #[test]
     fn test_parse_line_3() {
         let line = "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1";
         let score = process_line(line);
-        assert_eq!((2, 0), score);
+        assert_eq!((2, 2), score);
     }
 
     #[test]
     fn test_parse_line_4() {
         let line = "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83";
         let score = process_line(line);
-        assert_eq!((1, 0), score);
+        assert_eq!((1, 1), score);
     }
 
     #[test]
@@ -107,6 +122,6 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
     "#;
         let result = process_input(input.as_bytes());
-        assert_eq!((13, 0), result);
+        assert_eq!((13, 30), result);
     }
 }
