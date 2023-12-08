0f93facf5b96284922afc492ae92d5b0e8db8b3f
JD Huntington <jd@itsthehuntingtons.com>
2023-12-07 23:28:32 -0600

    [day08] first part done

--- advent-day08/src/main.rs
+++ advent-day08/src/main.rs
@@ -1,9 +1,17 @@
+use std::collections::HashMap;
 use std::io::{self, BufRead, Read};
 
 #[derive(Hash, Eq, PartialEq, Debug, Copy, Clone)]
 enum Direction {
     Left,
     Right,
+}
+
+#[derive(Debug, PartialEq)]
+struct MapNode {
+    name: String,
+    left: String,
+    right: String,
 }
 
 fn main() {
@@ -16,14 +24,26 @@ fn main() {
 
 fn process_input<R: Read>(reader: R) -> (u32, u32) {
     let buffered = io::BufReader::new(reader);
+    let mut directions: Option<Vec<Direction>> = None;
+    let mut map = HashMap::new();
     for line_result in buffered.lines() {
         let line = line_result.unwrap();
         if line.trim().is_empty() {
             continue;
         }
+        if directions.is_none() {
+            directions = Some(parse_directions(&line));
+        } else {
+            let map_node = parse_map_row(&line);
+            map.insert(map_node.name.clone(), map_node);
+        }
     }
 
-    (0, 0)
+    let mut simple_steps = 0;
+    if let Some(real_directions) = directions {
+        simple_steps = compute_simple_steps(&real_directions, &map);
+    }
+    (simple_steps, 0)
 }
 
 fn parse_directions(input: &str) -> Vec<Direction> {
@@ -38,6 +58,37 @@ fn parse_directions(input: &str) -> Vec<Direction> {
     directions
 }
 
+fn parse_map_row(input: &str) -> MapNode {
+    let mut parts = input.split(" = ");
+    let name = parts.next().unwrap();
+    let mut directions = parts.next().unwrap().split(", ");
+    let left = directions.next().unwrap();
+    let left = &left[1..];
+    let right = directions.next().unwrap();
+    // remove ending paren from right
+    let right = &right[..right.len() - 1];
+    MapNode {
+        name: name.to_string(),
+        left: left.to_string(),
+        right: right.to_string(),
+    }
+}
+
+fn compute_simple_steps(directions: &Vec<Direction>, map: &HashMap<String, MapNode>) -> u32 {
+    let mut steps_taken: u32 = 0;
+    let mut current_node_name = "AAA";
+    while current_node_name != "ZZZ" {
+        let current_node = map.get(current_node_name).unwrap();
+        let direction = directions[(steps_taken as usize) % directions.len()];
+        match direction {
+            Direction::Left => current_node_name = &current_node.left,
+            Direction::Right => current_node_name = &current_node.right,
+        }
+        steps_taken += 1;
+    }
+    steps_taken
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -47,6 +98,20 @@ mod tests {
         let input = "RL";
         let result = parse_directions(input);
         assert_eq!(vec![Direction::Right, Direction::Left], result);
+    }
+
+    #[test]
+    fn test_parse_map_row() {
+        let input = "AAA = (BBB, CCC)";
+        let result: MapNode = parse_map_row(input);
+        assert_eq!(
+            MapNode {
+                name: "AAA".to_string(),
+                left: "BBB".to_string(),
+                right: "CCC".to_string(),
+            },
+            result
+        );
     }
 
     #[test]
